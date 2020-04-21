71b9baf5cdca2760e75f74ac220a954871fb99ea
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-21 00:32:43 -0500

    first commit

---
+++ src/main/java/com/tobias/saul/ConferenceSpringBootApp/ConferenceSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.ConferenceSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tobias/saul/ConferenceSpringBootApp/ConferenceSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.ConferenceSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
