ce4386cff756a48c53e63bde19beefdb3c018b59
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-21 21:47:30 -0500

    added CRUD operations for Speaker and Session

--- src/main/java/com/tobias/saul/controllers/SessionsController.java
+++ src/main/java/com/tobias/saul/controllers/SessionsController.java
@@ -2,6 +2,7 @@ package com.tobias.saul.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +41,17 @@ public class SessionsController {
 		return sessionRepository.saveAndFlush(session);
 	}
 	
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable Long id) {
+		//(add this ->)also need to check for children records before deleting
+		sessionRepository.deleteById(id);
+	}
 	
+	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+	public Session update(@PathVariable Long id, @RequestBody Session session) {
+		Session existingSession = sessionRepository.getOne(id);
+		BeanUtils.copyProperties(session, existingSession, "session_id");;
+		return sessionRepository.saveAndFlush(existingSession);
+	}
 
 }
