6e282431f5a253ce014fc59eec3276a4426fcf36
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-21 20:41:53 -0500

    added entity models Speaker and Session

---
+++ src/main/java/com/tobias/saul/models/Session.java
@@ -0,0 +1,106 @@
+package com.tobias.saul.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "sessions")
+public class Session {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "session_id")
+	private Long sessionId;
+	@Column(name = "session_name")
+	private String sessionName;
+	@Column(name = "session_description")
+	private String sessionDescription;
+	@Column(name = "session_length")
+	private Integer sessionLength;
+	
+	//default constructor to help with serialization
+	public Session() {}
+
+	public Long getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(Long sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	public String getSessionName() {
+		return sessionName;
+	}
+
+	public void setSessionName(String sessionName) {
+		this.sessionName = sessionName;
+	}
+
+	public String getSessionDescription() {
+		return sessionDescription;
+	}
+
+	public void setSessionDescription(String sessionDescription) {
+		this.sessionDescription = sessionDescription;
+	}
+
+	public Integer getSessionLength() {
+		return sessionLength;
+	}
+
+	public void setSessionLength(Integer sessionLength) {
+		this.sessionLength = sessionLength;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((sessionDescription == null) ? 0 : sessionDescription.hashCode());
+		result = prime * result + ((sessionId == null) ? 0 : sessionId.hashCode());
+		result = prime * result + sessionLength;
+		result = prime * result + ((sessionName == null) ? 0 : sessionName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Session other = (Session) obj;
+		if (sessionDescription == null) {
+			if (other.sessionDescription != null)
+				return false;
+		} else if (!sessionDescription.equals(other.sessionDescription))
+			return false;
+		if (sessionId == null) {
+			if (other.sessionId != null)
+				return false;
+		} else if (!sessionId.equals(other.sessionId))
+			return false;
+		if (sessionLength != other.sessionLength)
+			return false;
+		if (sessionName == null) {
+			if (other.sessionName != null)
+				return false;
+		} else if (!sessionName.equals(other.sessionName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Session [sessionId=" + sessionId + ", sessionName=" + sessionName + ", sessionDescription="
+				+ sessionDescription + ", sessionLength=" + sessionLength + "]";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/tobias/saul/models/Speaker.java
@@ -0,0 +1,136 @@
+package com.tobias.saul.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "speakers")
+public class Speaker {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name= "speaker_id")
+	private Long speakerId;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	private String title;
+	private String company;
+	@Column(name = "speaker_bio")
+	private String speakerBio;
+	
+	public Speaker() {}
+
+	public Long getSpeakerId() {
+		return speakerId;
+	}
+
+	public void setSpeakerId(Long speakerId) {
+		this.speakerId = speakerId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getSpeakerBio() {
+		return speakerBio;
+	}
+
+	public void setSpeakerBio(String speakerBio) {
+		this.speakerBio = speakerBio;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((speakerBio == null) ? 0 : speakerBio.hashCode());
+		result = prime * result + ((speakerId == null) ? 0 : speakerId.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Speaker other = (Speaker) obj;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (speakerBio == null) {
+			if (other.speakerBio != null)
+				return false;
+		} else if (!speakerBio.equals(other.speakerBio))
+			return false;
+		if (speakerId == null) {
+			if (other.speakerId != null)
+				return false;
+		} else if (!speakerId.equals(other.speakerId))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Speaker [speakerId=" + speakerId + ", firstName=" + firstName + ", lastName=" + lastName + ", title="
+				+ title + ", company=" + company + ", speakerBio=" + speakerBio + "]";
+	}
+	
+	
+}
